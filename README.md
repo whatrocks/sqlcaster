a94d2859631ffb56134733a347641920c5e2b72c
Charlie Harrington <charles.harrington@gmail.com>
2022-10-11 09:15:02 -0700

    prettier and some cleanup

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Try now: https://sqlcaster.xyz/
 ## Developing locally:
 
 This repo is a pure client side web app so you can either just open `index.html` in your browser or if you prefer serve this folder locally using something like `python3 -m http.server`.
+
+### Lint
+
+```
+npx prettier --write .
+```
--- sqlcaster.js
+++ sqlcaster.js
@@ -1,86 +1,86 @@
-// SQLcaster
 let CURRENT_RESULTS = [];
-document.addEventListener("DOMContentLoaded", function () {
-  const BASE_URL = "https://sqlcaster.whatrocks.repl.co/api/";
+const BASE_URL = "https://sqlcaster.whatrocks.repl.co/api/";
 
-  function printBooks(books) {
-    const booklist = document.getElementById("booklist");
-    booklist.innerHTML = "";
-    if (Array.isArray(books)) {
-      for (let book of books) {
-        prettyPrintBook(book);
-      }
-    } else {
-      for (let field of books.casts.fields) {
-        printSchema(field);
-      }
+function printResults(books) {
+  const booklist = document.getElementById("booklist");
+  booklist.innerHTML = "";
+  if (Array.isArray(books)) {
+    for (let book of books) {
+      prettyPrint(book);
+    }
+  } else {
+    for (let field of books.casts.fields) {
+      printSchema(field);
     }
   }
+}
 
-  function printSchema(field) {
-    const booklist = document.getElementById("booklist");
-    const bookElement = document.createElement("div");
-    bookElement.classList = ["card"];
-    const p = document.createElement("p");
-    p.innerText = `${field.name}: ${field.data_type}`;
-    bookElement.appendChild(p);
-    booklist.appendChild(bookElement);
-  }
+function printSchema(field) {
+  const booklist = document.getElementById("booklist");
+  const bookElement = document.createElement("div");
+  bookElement.classList = ["card"];
+  const p = document.createElement("p");
+  p.innerText = `${field.name}: ${field.data_type}`;
+  bookElement.appendChild(p);
+  booklist.appendChild(bookElement);
+}
 
-  function prettyPrintBook(book) {
-    const booklist = document.getElementById("booklist");
-    const bookElement = document.createElement("div");
-    bookElement.classList = ["card"];
-    Object.keys(book).forEach((key) => {
-      switch (key) {
-        case "avatar_url":
-          const cover = document.createElement("img");
-          cover.classList = ["avatar"];
-          cover.alt = `${book.username}`;
-          cover.src = `${book.avatar_url}`;
-          bookElement.appendChild(cover);
-          break;
-        case "searchcaster_url":
-          const href = book.searchcaster_url;
-          if (!href) break;
-          const a = document.createElement("a");
-          a.href = href;
-          a.innerText = "View on Searchcaster";
-          a.target = "_blank";
-          bookElement.appendChild(a);
-          break;
-        default:
-          const p = document.createElement("p");
-          if (key.startsWith("COUNT")) {
-            p.innerText = `Count: ${book[key]}`;
-          } else {
-            p.innerText = `${key}: ${book[key]}`;
-          }
-          bookElement.appendChild(p);
-      }
-    });
-    booklist.appendChild(bookElement);
-  }
-
-  function fetchBooks(query, cb, query_type = "sql") {
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-      if (xmlhttp.readyState === XMLHttpRequest.DONE) {
-        const status = xmlhttp.status;
-        if (status === 0 || (status >= 200 && status < 400)) {
-          const results = JSON.parse(xmlhttp.responseText);
-          CURRENT_RESULTS = results;
-          cb(results);
+function prettyPrint(book) {
+  const booklist = document.getElementById("booklist");
+  const bookElement = document.createElement("div");
+  bookElement.classList = ["card"];
+  Object.keys(book).forEach((key) => {
+    switch (key) {
+      case "avatar_url":
+        const cover = document.createElement("img");
+        cover.classList = ["avatar"];
+        cover.alt = `${book.username}`;
+        cover.src = `${book.avatar_url}`;
+        bookElement.appendChild(cover);
+        break;
+      case "searchcaster_url":
+        const href = book.searchcaster_url;
+        if (!href) break;
+        const a = document.createElement("a");
+        a.href = href;
+        a.innerText = "View on Searchcaster";
+        a.target = "_blank";
+        bookElement.appendChild(a);
+        break;
+      default:
+        const p = document.createElement("p");
+        if (key.startsWith("COUNT")) {
+          p.innerText = `Count: ${book[key]}`;
         } else {
-          const booklist = document.getElementById("booklist");
-          booklist.innerHTML = "That didn't work";
+          p.innerText = `${key}: ${book[key]}`;
         }
+        bookElement.appendChild(p);
+    }
+  });
+  booklist.appendChild(bookElement);
+}
+
+function fetchBooks(query, cb, query_type = "sql") {
+  const xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState === XMLHttpRequest.DONE) {
+      const status = xmlhttp.status;
+      if (status === 0 || (status >= 200 && status < 400)) {
+        const results = JSON.parse(xmlhttp.responseText);
+        CURRENT_RESULTS = results;
+        cb(results);
+      } else {
+        const booklist = document.getElementById("booklist");
+        booklist.innerHTML = "That didn't work";
       }
-    };
-    const http_verb = query_type === "sql" ? "POST" : "GET";
-    xmlhttp.open(http_verb, `${BASE_URL}${query_type}`, true);
-    xmlhttp.send(query);
-  }
+    }
+  };
+  const http_verb = query_type === "sql" ? "POST" : "GET";
+  xmlhttp.open(http_verb, `${BASE_URL}${query_type}`, true);
+  xmlhttp.send(query);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
   const library = document.getElementById("the-library");
   const QUERIES = [
     "select * from casts where username = 'whatrocks' and reactions is not null order by reactions desc limit 3;",
@@ -95,12 +95,11 @@ document.addEventListener("DOMContentLoaded", function () {
     if (params.sql) {
       const queryInput = document.getElementById("library-input");
       queryInput.innerText = params.sql;
-      fetchBooks(params.sql, printBooks);
+      fetchBooks(params.sql, printResults);
     }
 
     // to set new query
     const newParams = new URLSearchParams();
-
     const libraryButton = document.getElementById("library-button");
     libraryButton.addEventListener("click", (e) => {
       e.preventDefault();
@@ -108,7 +107,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const query = queryInput.innerText.trim();
       newParams.set("sql", query);
       window.history.replaceState({}, "", `${location.pathname}?${newParams}`);
-      fetchBooks(query, printBooks);
+      fetchBooks(query, printResults);
     });
     const lazyButton = document.getElementById("library-lazy");
     lazyButton.addEventListener("click", (e) => {
@@ -119,14 +118,13 @@ document.addEventListener("DOMContentLoaded", function () {
       const query = queryInput.innerText.trim();
       newParams.set("sql", query);
       window.history.replaceState({}, "", `${location.pathname}?${newParams}`);
-      fetchBooks(query, printBooks);
+      fetchBooks(query, printResults);
     });
     const schemaButton = document.getElementById("library-schema");
     schemaButton.addEventListener("click", (e) => {
-      fetchBooks("", printBooks, "schema");
+      fetchBooks("", printResults, "schema");
     });
     const downloadButton = document.getElementById("download");
-
     downloadButton.addEventListener("click", (evt) => {
       window.open(
         encodeURI(
